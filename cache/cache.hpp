927a9f712d9ab1ecbd97f324639d2faa1c113a8e
Dmitry <nechda6@gmail.com>
2021-10-04 00:36:19 +0300

    Rewrited tests via templates & clang-force-format

--- cache/cache.hpp
+++ cache/cache.hpp
@@ -1,203 +1,210 @@
 #pragma once
-#include <cstddef>
 #include <cassert>
-#include <list>
-#include <vector>
-#include <unordered_map>
-#include <set>
-#include <unordered_set>
-#include <stack>
+#include <cstddef>
 #include <iostream>
+#include <list>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 #define UNRECHEABLE() assert(!"This line should be unrecheable.");
 
-namespace caches
-{
-    template <typename Key_t, typename Val_t>
-    class LRU_t{
-        public:
-            LRU_t(const LRU_t<Key_t, Val_t>&) = delete;
-            LRU_t(size_t size) : size_(size) {}
-            
-            bool look_update(Key_t key, Val_t&& val = Val_t()) {
-                auto hit = hash_.find(key);
+namespace caches {
+template <typename Key_t, typename Val_t> class LRU_t {
+public:
+    LRU_t(const LRU_t<Key_t, Val_t> &) = delete;
+    LRU_t(LRU_t<Key_t, Val_t> &&) = default;
+    LRU_t(size_t size) : size_(size) {}
 
-                if (hit == hash_.end()) {
-                    if (full()) {
-                        hash_.erase(cache_.back());
-                        cache_.pop_back();
-                    }
-                    cache_.push_front(std::move(val));
-                    hash_[key] = cache_.begin();
-                    return false;
-                }
+    bool look_update(Key_t key, Val_t &&val = Val_t()) {
+        auto hit = hash_.find(key);
 
-                auto eltit = hit->second;
-                if (eltit != cache_.begin())
-                    cache_.splice(cache_.begin(), cache_, eltit, std::next(eltit));
-                return true;
+        if (hit == hash_.end()) {
+            if (full()) {
+                hash_.erase(cache_.back());
+                cache_.pop_back();
+            }
+            cache_.push_front(std::move(val));
+            hash_[key] = cache_.begin();
+            return false;
+        }
+
+        auto eltit = hit->second;
+        if (eltit != cache_.begin())
+            cache_.splice(cache_.begin(), cache_, eltit, std::next(eltit));
+        return true;
+    }
+
+    ~LRU_t() = default;
+
+private:
+    size_t size_;
+    using ListIt_t = typename std::list<Val_t>::iterator;
+    std::list<Val_t> cache_;
+    std::unordered_map<Key_t, ListIt_t> hash_;
+
+    bool full() { return cache_.size() == size_; }
+};
+
+template <typename Key_t, typename Val_t> class LFU_t {
+public:
+    LFU_t(const LRU_t<Key_t, Val_t> &) = delete;
+    LFU_t(LFU_t<Key_t, Val_t> &&) = default;
+    LFU_t(size_t size) : size_(size), min_freq_(1), n_elemets_(0) {}
+
+    bool look_update(Key_t key, Val_t &&val = Val_t()) {
+        if (hash_map_.find(key) != hash_map_.end()) {
+            auto it = hash_map_[key];
+            auto freq = it->freq++;
+
+            freq_map_[freq + 1].push_front(std::move(*it));
+            freq_map_[freq].erase(it);
+            if (freq_map_[freq].empty()) {
+                freq_map_.erase(freq);
+                min_freq_ += freq == min_freq_;
             }
 
-            ~LRU_t() = default;
-        private:
-            size_t size_;
-            using ListIt_t = typename std::list<Val_t>::iterator;
-            std::list<Val_t> cache_;
-            std::unordered_map<Key_t, ListIt_t> hash_;
+            hash_map_[key] = freq_map_[freq + 1].begin();
 
-            bool full() { return cache_.size() == size_; }
-    }; 
+            return true;
+        }
 
-    template <typename Key_t, typename Val_t>
-    class LFU_t {
-        public:
-            LFU_t(const LRU_t<Key_t, Val_t>&) = delete;
-            LFU_t(size_t size) :
-                size_(size), min_freq_(1),
-                n_elemets_(0)
-            {}
-            
-            bool look_update(Key_t key, Val_t&& val = Val_t()) {
-                if(hash_map_.find(key) != hash_map_.end()) {
-                    auto it = hash_map_[key];
-                    auto freq = it->freq++;
+        if (n_elemets_ >= size_) {
+            auto key = freq_map_[min_freq_].back().key;
+            hash_map_.erase(key);
+            freq_map_[min_freq_].pop_back();
+            if (freq_map_[min_freq_].empty())
+                freq_map_.erase(min_freq_);
+            n_elemets_--;
+        }
 
-                    freq_map_[freq + 1].push_front(std::move(*it));
-                    freq_map_[freq].erase(it);
-                    if(freq_map_[freq].empty()) {
-                        freq_map_.erase(freq);
-                        min_freq_ += freq == min_freq_;
-                    }
+        n_elemets_++;
+        min_freq_ = 1;
+        freq_map_[min_freq_].push_front(std::move(Node_t{key, std::move(val), min_freq_}));
+        hash_map_[key] = freq_map_[min_freq_].begin();
 
-                    hash_map_[key] = freq_map_[freq + 1].begin();
+        return false;
+    }
 
-                    return true;
-                }
+    void dump() const {
+        for (auto &f : freq_map_) {
+            std::cout << f.first << "\n";
+            for (auto &it : f.second)
+                std::cout << it.key << ", ";
+            std::cout << std::endl;
+        }
+    }
 
-                if(n_elemets_ >= size_) {
-                    auto key = freq_map_[min_freq_].back().key;
-                    hash_map_.erase(key);
-                    freq_map_[min_freq_].pop_back();
-                    if(freq_map_[min_freq_].empty())
-                        freq_map_.erase(min_freq_);
-                    n_elemets_--;
-                }
+    ~LFU_t() = default;
 
-                n_elemets_++;
-                min_freq_ = 1;
-                freq_map_[min_freq_].push_front(std::move(Node_t{key, std::move(val), min_freq_}));
-                hash_map_[key] = freq_map_[min_freq_].begin();
-
-                return false;
-            }
-
-            void dump() const {
-                for(auto& f : freq_map_) {
-                    std::cout << f.first << "\n";
-                    for(auto& it : f.second)
-                        std::cout << it.key << ", ";
-                    std::cout<< std::endl;
-                }
-            }
-
-            ~LFU_t() = default;
-        private:
-            struct Node_t {
-                Key_t key;
-                Val_t val;
-                size_t freq;
-            };
-            size_t size_;
-            size_t min_freq_;
-            size_t n_elemets_;
-
-            using List_t = typename std::list<Node_t>;
-            using ListIt_t = typename std::list<Node_t>::iterator;
-            std::unordered_map<Key_t, ListIt_t> hash_map_;
-            std::unordered_map<size_t, List_t> freq_map_;
-    }; 
-
-    template <typename Key_t, typename Val_t>
-    class perfect_t {
-        public:
-            perfect_t(const perfect_t<Key_t, Val_t>&) = delete;
-            perfect_t(size_t size, std::vector<Key_t>& req) :
-                size_(size),
-                req_(req)
-            {}
-            
-            bool look_update(Key_t key, Val_t&& val = Val_t()) {
-                UNRECHEABLE();
-            }
-
-            size_t misses_amount() const {
-                Cache_table_t cache_table;
-                size_t N_iterations = req_.size();
-                size_t total_misses = 0;
-                std::vector<int> cached_keys(size_);
-                cached_keys.resize(0);
-
-                for(size_t i = 0; i < N_iterations; i++) {
-                    int cur_key = req_[i];
-                    if(cache_table.count(cur_key) == 1)
-                        continue;
-                    total_misses++;
-                    size_t min_misses = -1;
-                    size_t tmp_misses = -1;
-
-                    // without cache new element
-                    tmp_misses = misses_at_current_cache(cache_table, i);
-                    min_misses = std::min(min_misses, tmp_misses);
-
-                    // transform unordered_set into vector
-                    cached_keys.resize(0);
-                    for(const auto& it : cache_table)
-                        cached_keys.push_back(it);
-                    
-                    // if there is free space just cache a new key
-                    if(cached_keys.size() < size_) {
-                        cache_table.insert(cur_key);
-                        continue;
-                    }
-
-                    // find the most gainful place in cache table
-                    int replaced_key = -1;
-                    for(auto key : cached_keys) {
-                        cache_table.erase(key);
-                        cache_table.insert(cur_key);
-                        
-                        tmp_misses = misses_at_current_cache(cache_table, i);
-                        if(tmp_misses < min_misses)
-                            replaced_key = key;
-                        min_misses = std::min(min_misses, tmp_misses);
-                        
-                        cache_table.erase(cur_key);
-                        cache_table.insert(key);
-                    }
-
-                    // overwise replace item in the cache table
-                    if(replaced_key != -1) {
-                        cache_table.erase(replaced_key);
-                        cache_table.insert(cur_key);
-                        continue;
-                    }
-
-                }
-                return total_misses;
-            }
-
-            ~perfect_t() = default;
-        private:
-            size_t size_;
-            std::vector<int> req_;
-            using Cache_table_t = std::unordered_set<int>;
-            size_t misses_at_current_cache(const Cache_table_t& table, size_t from) const {
-                size_t hits = 0;
-                size_t N = req_.size();
-                for(size_t i = from; i < N; i++)
-                    hits += !table.count(req_[i]);
-                return hits;
-            }
+private:
+    struct Node_t {
+        Key_t key;
+        Val_t val;
+        size_t freq;
     };
+    size_t size_;
+    size_t min_freq_;
+    size_t n_elemets_;
 
-} // namespace cache
+    using List_t = typename std::list<Node_t>;
+    using ListIt_t = typename std::list<Node_t>::iterator;
+    std::unordered_map<Key_t, ListIt_t> hash_map_;
+    std::unordered_map<size_t, List_t> freq_map_;
+};
+
+template <typename Key_t, typename Val_t> class perfect_t {
+public:
+    perfect_t(const perfect_t<Key_t, Val_t> &) = delete;
+    perfect_t(perfect_t<Key_t, Val_t> &&) = default;
+    perfect_t(size_t size, std::vector<Key_t> &req) : size_(size), req_(req), current_request_index(0) {
+        hits_history_.resize(req.size());
+        misses_amount();
+    }
+
+    bool look_update(Key_t key, Val_t &&val = Val_t()) {
+        if (current_request_index >= req_.size()) {
+            UNRECHEABLE();
+            return false;
+        }
+
+        return hits_history_[current_request_index++];
+    }
+
+    size_t misses_amount() {
+        Cache_table_t cache_table;
+        size_t N_iterations = req_.size();
+        size_t total_misses = 0;
+        std::vector<int> cached_keys(size_);
+        cached_keys.resize(0);
+
+        for (size_t i = 0; i < N_iterations; i++) {
+            int cur_key = req_[i];
+            if (cache_table.count(cur_key) == 1) {
+                hits_history_[i] = 1;
+                continue;
+            }
+            total_misses++;
+            size_t min_misses = -1;
+            size_t tmp_misses = -1;
+
+            // without cache new element
+            tmp_misses = misses_at_current_cache(cache_table, i);
+            min_misses = std::min(min_misses, tmp_misses);
+
+            // transform unordered_set into vector
+            cached_keys.resize(0);
+            for (const auto &it : cache_table)
+                cached_keys.push_back(it);
+
+            // if there is free space just cache a new key
+            if (cached_keys.size() < size_) {
+                cache_table.insert(cur_key);
+                continue;
+            }
+
+            // find the most gainful place in cache table
+            int replaced_key = -1;
+            for (auto key : cached_keys) {
+                cache_table.erase(key);
+                cache_table.insert(cur_key);
+
+                tmp_misses = misses_at_current_cache(cache_table, i);
+                if (tmp_misses < min_misses)
+                    replaced_key = key;
+                min_misses = std::min(min_misses, tmp_misses);
+
+                cache_table.erase(cur_key);
+                cache_table.insert(key);
+            }
+
+            // overwise replace item in the cache table
+            if (replaced_key != -1) {
+                cache_table.erase(replaced_key);
+                cache_table.insert(cur_key);
+                continue;
+            }
+        }
+        return total_misses;
+    }
+
+    ~perfect_t() = default;
+
+private:
+    size_t size_;
+    std::vector<int> req_;
+    std::vector<bool> hits_history_;
+    size_t current_request_index;
+    using Cache_table_t = std::unordered_set<int>;
+    size_t misses_at_current_cache(const Cache_table_t &table, size_t from) const {
+        size_t hits = 0;
+        size_t N = req_.size();
+        for (size_t i = from; i < N; i++)
+            hits += !table.count(req_[i]);
+        return hits;
+    }
+};
+
+} // namespace caches
--- cache/main.cpp
+++ cache/main.cpp
@@ -1,164 +1,149 @@
-#include <iostream>
-#include <iomanip>
 #include "cache.hpp"
+#include <iomanip>
+#include <iostream>
 
 #define emit(var) std::cout << #var " = " << var << std::endl;
 
 using namespace std;
 
-
 static inline void print_test_title(size_t cur, size_t total, bool ok) {
     std::cout << "[" << cur << "/" << total << "] " << (ok ? "[PASSED]" : "[FAILED]\n");
     std::cout << (cur < total ? "\r" : "");
-    std::cout << (ok && cur == total ? "\n" : "") << std::flush;  
+    std::cout << (ok && cur == total ? "\n" : "") << std::flush;
 }
 
 struct Test_t {
-        size_t N;
-        std::vector<int> req;
-        size_t answ;
-    };
+    size_t N;
+    std::vector<int> req;
+    size_t answ;
+};
 
-void test_perfect_cache() {
+template <typename Cache_t> struct Make_cache {};
 
-    Test_t tests[] = {
-        {1, {1,1}, 1},
-        {1, {1,2}, 2},
-        {2, {1,1}, 1},
-        {2, {1,2}, 2},
-        {2, {1,1,2,2}, 2},
-        {2, {1,1,3,2,2,2,1}, 3},
-        {3, {1,1,3,2,2,2,1}, 3},
-        {3, {1,2,3,4,3,3,3,5,5,5,5,1}, 5},
-        {3, {1,2,3,4,4,4,5,5,5,7,6,6,6,6,6,1,4,4,4,4,5,5,5}, 8},
-        {4, {1,2,3,4,  5,  1,2,3,4,  6,  1,2,3,4,  7,  1,2,3,4,  8,  1,2,3,4}, 8},
-        {4, {1,2,3,4,  5,  1,2,3,4,  5,  1,2,3,4,  5,  1,2,3,4,  5,  1,2,3,4,  5, 5}, 9},
-    };
+template <typename K, typename V> struct Make_cache<caches::perfect_t<K, V>> {
+    static caches::perfect_t<K, V> get(Test_t &t) { return caches::perfect_t<K, V>(t.N, t.req); }
+};
 
-    std::cout << "Perfect cache testing" << std::endl;
+template <typename K, typename V> struct Make_cache<caches::LFU_t<K, V>> {
+    static caches::LFU_t<K, V> get(Test_t &t) { return caches::LFU_t<K, V>(t.N); }
+};
 
-    size_t N_tests = sizeof(tests)/sizeof(tests[0]);
-    for(size_t i = 0; i < N_tests; i++) {
-        caches::perfect_t<int, int> cache(tests[i].N, tests[i].req);
-        size_t answ = cache.misses_amount();
-        if(answ != tests[i].answ) {
-            print_test_title(i+1, N_tests, 0);
-            std::cout << "Cache size = " << tests[i].N << std::endl;
-            std::cout << "Requests = {";
-            for(auto it = tests[i].req.begin(); it != tests[i].req.end(); it++)
-                std::cout << *it << (std::next(it) == tests[i].req.end() ? "" : ", ");
-            std::cout << "};" << std::endl;
-            std::cout << "Misses got      = " << answ << std::endl
-                      << "Misses expected = " << tests[i].answ << std::endl;     
-            return;
-        }
-        print_test_title(i+1, N_tests, 1);
-    }
-}
+template <typename Cache_t, size_t N_tests> void test_cache(Test_t *tests, const char *title_msg) {
+    std::cout << title_msg << std::endl;
 
-void test_LFU_cache() {
-    
-    Test_t tests[] = {
-        {1, {1,1}, 1},
-        {1, {1,2}, 2},
-        {2, {1,1}, 1},
-        {2, {1,2}, 2},
-        {2, {1,1,2,2}, 2},
-        {2, {1,1,3,2,2,2,1}, 3},
-        {3, {1,1,3,2,2,2,1}, 3},
-        {3, {1,2,3,4,3,3,3,5,5,5,5,1}, 6},
-        {4, {1, 2, 1, 3, 2, 4, 5}, 5},
-        {3, {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2}, 9}
-    };
-
-    std::cout << "LFU cache testing" << std::endl;
-
-    size_t N_tests = sizeof(tests)/sizeof(tests[0]);
-    for(size_t i = 0; i < N_tests; i++) {
-        caches::LFU_t<int, int> cache(tests[i].N);
+    for (size_t i = 0; i < N_tests; i++) {
+        Cache_t cache = Make_cache<Cache_t>::get(tests[i]);
         size_t answ = 0;
-        for(const auto& it : tests[i].req) {
+        for (const auto &it : tests[i].req) {
             bool hit = cache.look_update(it);
             answ += !hit;
         }
-        if(answ != tests[i].answ) {
-            print_test_title(i+1, N_tests, 0);
+        if (answ != tests[i].answ) {
+            print_test_title(i + 1, N_tests, 0);
             std::cout << "Cache size = " << tests[i].N << std::endl;
             std::cout << "Requests = {";
-            for(auto it = tests[i].req.begin(); it != tests[i].req.end(); it++)
+            for (auto it = tests[i].req.begin(); it != tests[i].req.end(); it++)
                 std::cout << *it << (std::next(it) == tests[i].req.end() ? "" : ", ");
             std::cout << "};" << std::endl;
             std::cout << "Misses got      = " << answ << std::endl
-                      << "Misses expected = " << tests[i].answ << std::endl;     
+                      << "Misses expected = " << tests[i].answ << std::endl;
             return;
         }
-        print_test_title(i+1, N_tests, 1);
+        print_test_title(i + 1, N_tests, 1);
     }
+}
+
+void test_caches() {
+    Test_t tests_perfect[] = {
+        {1, {1, 1}, 1},
+        {1, {1, 2}, 2},
+        {2, {1, 1}, 1},
+        {2, {1, 2}, 2},
+        {2, {1, 1, 2, 2}, 2},
+        {2, {1, 1, 3, 2, 2, 2, 1}, 3},
+        {3, {1, 1, 3, 2, 2, 2, 1}, 3},
+        {3, {1, 2, 3, 4, 3, 3, 3, 5, 5, 5, 5, 1}, 5},
+        {3, {1, 2, 3, 4, 4, 4, 5, 5, 5, 7, 6, 6, 6, 6, 6, 1, 4, 4, 4, 4, 5, 5, 5}, 8},
+        {4, {1, 2, 3, 4, 5, 1, 2, 3, 4, 6, 1, 2, 3, 4, 7, 1, 2, 3, 4, 8, 1, 2, 3, 4}, 8},
+        {4, {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 5}, 9},
+    };
+    test_cache<caches::perfect_t<int, int>, sizeof(tests_perfect) / sizeof(Test_t)>(tests_perfect,
+                                                                                    "Perfect cache testing");
+
+    Test_t tests_LFU[] = {{1, {1, 1}, 1},
+                          {1, {1, 2}, 2},
+                          {2, {1, 1}, 1},
+                          {2, {1, 2}, 2},
+                          {2, {1, 1, 2, 2}, 2},
+                          {2, {1, 1, 3, 2, 2, 2, 1}, 3},
+                          {3, {1, 1, 3, 2, 2, 2, 1}, 3},
+                          {3, {1, 2, 3, 4, 3, 3, 3, 5, 5, 5, 5, 1}, 6},
+                          {4, {1, 2, 1, 3, 2, 4, 5}, 5},
+                          {3, {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2}, 9}};
+    test_cache<caches::LFU_t<int, int>, sizeof(tests_LFU) / sizeof(Test_t)>(tests_LFU, "LFU cache testing");
 }
 
 void cache_comparison() {
 
     Test_t tests[] = {
-        {1, {1,1}},
-        {1, {1,2}},
-        {2, {1,1}},
-        {2, {1,2}},
-        {2, {1,1,2,2}},
-        {2, {1,1,3,2,2,2,1}},
-        {3, {1,1,3,2,2,2,1}},
-        {3, {1,2,3,4,3,3,3,5,5,5,5,1}},
-        {3, {1,2,3,4,4,4,5,5,5,7,6,6,6,6,6,1,4,4,4,4,5,5,5}},
-        {4, {1,2,3,4,  5,  1,2,3,4,  6,  1,2,3,4,  7,  1,2,3,4,  8,  1,2,3,4}},
-        {4, {1,2,3,4,  5,  1,2,3,4,  5,  1,2,3,4,  5,  1,2,3,4,  5,  1,2,3,4,  5, 5}},
+        {1, {1, 1}},
+        {1, {1, 2}},
+        {2, {1, 1}},
+        {2, {1, 2}},
+        {2, {1, 1, 2, 2}},
+        {2, {1, 1, 3, 2, 2, 2, 1}},
+        {3, {1, 1, 3, 2, 2, 2, 1}},
+        {3, {1, 2, 3, 4, 3, 3, 3, 5, 5, 5, 5, 1}},
+        {3, {1, 2, 3, 4, 4, 4, 5, 5, 5, 7, 6, 6, 6, 6, 6, 1, 4, 4, 4, 4, 5, 5, 5}},
+        {4, {1, 2, 3, 4, 5, 1, 2, 3, 4, 6, 1, 2, 3, 4, 7, 1, 2, 3, 4, 8, 1, 2, 3, 4}},
+        {4, {1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 5}},
     };
 
     std::cout << "Cache comparison (misses)" << std::endl;
-    size_t N_tests = sizeof(tests)/sizeof(tests[0]);
-    for(size_t i = 0; i < N_tests; i++) {
+    size_t N_tests = sizeof(tests) / sizeof(tests[0]);
+    for (size_t i = 0; i < N_tests; i++) {
         caches::perfect_t<int, int> cache_0(tests[i].N, tests[i].req);
         caches::LFU_t<int, int> cache_1(tests[i].N);
 
-        size_t misses_0 = cache_0.misses_amount();
+        size_t misses_0 = 0;
         size_t misses_1 = 0;
-        for(const auto& it : tests[i].req) {
+        for (const auto &it : tests[i].req) {
+            misses_0 += !cache_0.look_update(it);
             misses_1 += !cache_1.look_update(it);
         }
 
-        std::cout << std::setw(8) << "perfect = "
-                  << std::setw(3) << misses_0
-                  << std::setw(8) << "LFU = "
-                  << std::setw(3) << misses_1 << std::endl;
+        std::cout << std::setw(8) << "perfect = " << std::setw(3) << misses_0 << std::setw(8)
+                  << "LFU = " << std::setw(3) << misses_1 << std::endl;
     }
 }
 
 int main() {
-    #ifdef TESTS
-        test_perfect_cache();
-        test_LFU_cache();
-        cache_comparison();
-    #elif LFU
-        size_t cache_size = 0;
-        size_t req_amount = 0;
-        std::cin >> cache_size >> req_amount;
-        caches::LFU_t<int, int> cache(cache_size);
+#ifdef TESTS
+    test_caches();
+    cache_comparison();
+#elif LFU
+    size_t cache_size = 0;
+    size_t req_amount = 0;
+    std::cin >> cache_size >> req_amount;
+    caches::LFU_t<int, int> cache(cache_size);
 
-        size_t hits = 0;
-        for(size_t i = 0; i < req_amount; i++) {
-            int req = 0;
-            std::cin >> req;
-            hits += cache.look_update(req);
-        }
-        std::cout << hits << std::endl;
-    #elif PERFECT
-        size_t cache_size = 0;
-        size_t req_amount = 0;
-        std::cin >> cache_size >> req_amount;
-        std::vector<int> req(req_amount);
-        for(auto& r : req)
-            std::cin >> r;
-        caches::perfect_t<int, int> cache(cache_size, req);
-        size_t hits = req_amount - cache.misses_amount();
-        std::cout << hits << std::endl;
-    #endif
+    size_t hits = 0;
+    for (size_t i = 0; i < req_amount; i++) {
+        int req = 0;
+        std::cin >> req;
+        hits += cache.look_update(req);
+    }
+    std::cout << hits << std::endl;
+#elif PERFECT
+    size_t cache_size = 0;
+    size_t req_amount = 0;
+    std::cin >> cache_size >> req_amount;
+    std::vector<int> req(req_amount);
+    for (auto &r : req)
+        std::cin >> r;
+    caches::perfect_t<int, int> cache(cache_size, req);
+    size_t hits = req_amount - cache.misses_amount();
+    std::cout << hits << std::endl;
+#endif
     return 0;
 }
